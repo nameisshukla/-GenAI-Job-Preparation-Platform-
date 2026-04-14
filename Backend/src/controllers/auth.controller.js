3bff5280018f0c5401bfbccac2c599e10830ad20
Aman Shukla <amanshukla9817@gmail.com>
2026-04-14 23:04:25 +0530

    Fix: Cross-site cookie (SameSite/Secure) configuration

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -43,7 +43,11 @@ async function registerUserController(req, res) {
             { expiresIn: "1d" }
          )
 
-         res.cookie("token",token)
+         res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "None"
+         })
 
          res.status(201).json({
             message: "User registered successfully",
@@ -88,7 +92,11 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None"
+    })
     res.status(200).json({
         message: "User loggedIn successfully.",
         user: {
@@ -114,7 +122,11 @@ async function logoutUserController(req, res) {
         await tokenBlacklistModel.create({ token })
     }
 
-    res.clearCookie("token")
+    res.clearCookie("token", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None"
+    })
 
     res.status(200).json({
         message: "User logged out successfully"
